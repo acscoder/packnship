6d0bd9e2a6382bdd4734877cffb764fa1a4c9c02
Ann Lee <acscoder@gmail.com>
2022-04-04 08:06:45 +0700

    updated

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default class MyDocument extends Document {
   render() {
@@ -7,7 +8,7 @@ export default class MyDocument extends Document {
         <Head>
       
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet"/>
-         
+          <script src="./js/feedback.js"></script> 
         </Head>
         <body className="h-full font-light text-base 2xl:text-xl bg-cascade transition">
           <Main />
---
+++ public/js/feedback.js
@@ -0,0 +1,8 @@
+(function (d, t, g) {
+  var ph    = d.createElement(t), s = d.getElementsByTagName(t)[0];
+  ph.type   = 'text/javascript';
+  ph.async   = true;
+  ph.charset = 'UTF-8';
+  ph.src     = g + '&v=' + (new Date()).getTime();
+  s.parentNode.insertBefore(ph, s);
+})(document, 'script', '//comments.digitalonda.com/?p=3030&ph_apikey=9f62ffdae81ab02f56610cf13699d680');
