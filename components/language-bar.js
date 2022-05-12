7d09ede48e81584d82bae5fff6ff30ecdb688394
Ann Lee <acscoder@gmail.com>
2022-05-12 16:37:32 +0700

    update

--- components/language-bar.js
+++ components/language-bar.js
@@ -1,8 +1,8 @@
 import Link from 'next/link'
 export default function LanguageBar({currentSlug,locale}) {   
     return ( 
-    <>
+    <div className="hidden">
     <Link href={currentSlug} locale="de"><a className={locale=="de" ?"font-bold":"hover:font-bold"}>DE</a></Link>/<Link href={currentSlug} locale="en"><a className={locale=="en" ?"font-bold":"hover:font-bold"}>EN</a></Link>
-    </>
+    </div>
     )
 }
