abd33f5e79d17cd24808582cc67ce935a87c794d
Ann Lee <acscoder@gmail.com>
2022-05-26 19:45:39 +0700

    update

--- components/footer.js
+++ components/footer.js
@@ -9,18 +9,18 @@ export default function Footer({address,email,hotline}) {
     <div className="border-4 border-black relative lg:h-[156px]">
         <div className="2xl:px-[105px] xl:px-[80px] lg:px-[40px] px-6 lg:mt-[18px]">
             <div className="lg:grow lg:mr-[320px]">
-                <div className="md:grid md:grid-cols-3 lg:gap-x-10 xl:grid-cols-4 xl:gap-x-20 md:inline-flex h-full items-center md:text-left text-center lg:py-0 py-5">
-                    <div className="2xl:col-span-2 col-span-1 py-2">
+                <div className="md:grid md:grid-cols-3 lg:gap-x-10 xl:gap-x-20 md:inline-flex h-full items-center md:text-left text-center lg:py-0 py-5">
+                    <div className="col-span-1 py-2">
                         <div className="logo">
                         <Link href="/">
                         <a className="2xl:text-6xl xl:text-5xl lg:text-4xl text-3xl"><span className="icon-logo"></span></a>
                         </Link>
                        </div>
-                       <div className="font-base"><a href={"/sitemap.xml"} target="_blank">Sitemap</a> | <Link href="/impressum"><a>Impressum</a></Link> | <a href="https://www.linkedin.com/company/packnship/?trk=public_jobs_topcard-org-name&amp;originalSubdomain=de" target="_blank" rel="noopener">Linkedin</a></div>
+                       <div className="2xl:text-3xl xl:text-2xl text-xl">Beautiful Logistics</div>
                     
                     </div>
                     <div className="col-span-1 py-2">
-                        <div className="h-full inline-flex items-center">
+                        <div className="h-full inline-flex items-center xl:ml-20">
                             <div className="text-base 3xl:text-2xl 2xl:text-xl">
                             {parse(address)}
                             </div>
@@ -29,16 +29,20 @@ export default function Footer({address,email,hotline}) {
                     <div className="col-span-1 py-2">
                         <div className="h-full inline-flex items-center">
                             <div className="text-base 3xl:text-2xl 2xl:text-xl">
-                                <a href={"mailto:"+email} className="" rel="nofollow">{email}</a><br/>
-                                <a href={"tel:"+hotline} className="" rel="nofollow">{hotline}</a>
+                                <div><span className="icon-mail mr-2"></span><a href={"mailto:"+email} className="" rel="nofollow">{email}</a></div>
+                                <div><span className="icon-mobile mr-2"></span><a href={"tel:"+hotline} className="" rel="nofollow">{hotline}</a></div>
+                                <div><span className="icon-megaphone mr-2"></span><a href="https://www.linkedin.com/company/packnship/?trk=public_jobs_topcard-org-name&amp;originalSubdomain=de" rel="nofollow" target="_blank">Linkedin</a></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>     
+            <div className="lg:absolute top-0 right-0 block grow-0 text-center lg:w-80 w-full lg:border-l-[3px] lg:border-t-0 border-t-[3px] border-black h-full">
             <Link href="/contact-us">
-            <a className="lg:absolute top-0 right-0 block grow-0 text-center lg:w-80 w-full lg:text-3xl text-2xl font-normal lg:border-l-[3px] lg:border-t-0 border-t-[3px] border-black lg:py-14 py-8 hover:bg-cascade transition-colors">Let’s Talk</a>
+            <a className="lg:text-3xl text-2xl font-normal pb-2 block mt-10">Let’s Talk</a>
             </Link>
+            <div className="text-base"><Link href="/sitemap"><a>Sitemap</a></Link> | <Link href="/impressum"><a>Impressum</a></Link> | <a href="https://1000k.berlin/" target="_blank" rel="nofollow">Site: 1000K.berlin</a></div>
+            </div>
         </div>
     </div>
     </div>
