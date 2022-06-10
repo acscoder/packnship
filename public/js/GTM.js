d811f7c321f54d54d6b4e4bb0ad9cb445e7651f7
Ann Lee <acscoder@gmail.com>
2022-06-10 14:34:33 +0700

    update

--- public/js/GTM.js
+++ public/js/GTM.js
@@ -1,5 +1,22 @@
+function readCookie(name) {
+    var nameEQ = escape(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return unescape(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+} ;
+
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-MCTCDRR');
+
+dataLayer.push({'event': 'userAllowCookie',
+'CookieConsentAcceptAnalytics':readCookie("CookieConsentAcceptAnalytics"),
+'CookieConsentAcceptEssential':readCookie("CookieConsentAcceptEssential"),
+'CookieConsentAcceptAdvertising':readCookie("CookieConsentAcceptAdvertising")
+})
